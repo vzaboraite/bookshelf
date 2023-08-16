51be909a52f966cc6efc0e8ec35b6a59742d7350
Viktorija Zaboraite <25870341+vzaboraite@users.noreply.github.com>
2023-08-16 18:43:26 +0100

    exercises/01-bootstrap - Add solution to the main exercise: - rename index.exercise.js to index.exercise.jsx - render the logo, title and the buttons

--- src/index.exercise.jsx
+++ src/index.exercise.jsx
@@ -1,9 +1,23 @@
 // 🐨 you'll need to import react and createRoot from react-dom up here
+import React from 'react'
+import {createRoot} from 'react-dom/client'
 
 // 🐨 you'll also need to import the Logo component from './components/logo'
+import {Logo} from 'components/logo'
 
 // 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
 // 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
-
+function App() {
+  return (
+    <div>
+      <Logo />
+      <h1>Bookshelf</h1>
+      <button>Login</button>
+      <button>Register</button>
+    </div>
+  )
+}
 // 🐨 use createRoot to render the <App /> to the root element
 // 💰 find the root element with: document.getElementById('root')
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
